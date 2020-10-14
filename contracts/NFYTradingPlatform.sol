57ae7d5e0b8d315ca666036f4aafa3668f2acb7a
JExtor <JeffreyJExtor@gmail.com>
2020-10-14 11:12:25 -0400

    Working on orders for trading platform

--- contracts/NFYTradingPlatform.sol
+++ contracts/NFYTradingPlatform.sol
@@ -1,22 +1,42 @@
 pragma solidity 0.6.10;
-pragma experimental ABIEncoderV2;
 
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/math/SafeMath.sol';
-import "./Ownable.sol";
+import "@openzeppelin/contracts/GSN/Context.sol";
+import './Ownable.sol';
 
 contract NFYTradingPlatform is Ownable {
     using SafeMath for uint;
+
+    modifier stakeNFTExist(bytes32 _ticker) {
+        require(tokens[ticker].tokenAddress != address(0), "staking NFT does not exist");
+        _;
+    }
+
+    enum Side {
+        BUY,
+        SELL
+    }
 
     struct StakeToken {
         bytes32 ticker;
         address tokenAddress;
     }
 
+    struct Order {
+        uint id;
+        Side side;
+        bytes32 ticker;
+        uint amount;
+        uint filled;
+        uint price;
+        uint date;
+    }
+
     mapping(bytes32 => StakeToken) public tokens;
     bytes32[] public stakeTokenList;
-    mapping(address => mapping(bytes32 => uint)) public traderBalances;
-
+    mapping(address => mapping(bytes32 => uint)) public traderNFT;
+    
     // Function that adds staking NFT
     function addToken( bytes32 _ticker, address _tokenAddress) onlyOwner() external {
         tokens[_ticker] = StakeToken(_ticker, _tokenAddress);
@@ -24,10 +44,22 @@ contract NFYTradingPlatform is Ownable {
     }
 
     // Function that allows user to deposit staking NFT
-    function depositNFT(uint _tokenId, bytes32 _ticker) external {
-
+    function depositNFT(uint _tokenId, bytes32 _ticker) stakeNFTExist(_ticker) external {
+        IERC721(tokens[_ticker].tokenAddress).transferFrom(
+            _msgSender(), address(this), _tokenId
+        );
+        traderNFT[_msgSender()][_ticker] = _tokenId;
     }
-    
+
+    // Function that allows a user to withdraw their staking NFT
+    function withdrawNFT(uint _tokenId, bytes32 _ticker) stakeNFTExist(_ticker) external {
+        require(traderNFT[_msgSender()][_ticker] == _tokenId, "User does not have this staking NFT");
+
+        traderNFT[_msgSender()][_ticker] = 0;
+
+        IERC721(tokens[_ticker].tokenAddress).transfer(address(this), _tokenId);
+    }
+
     function sellStake() public {
     }
 
