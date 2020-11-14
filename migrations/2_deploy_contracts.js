58d5eab9ae9499a619fa85c0a7287fe72882e99f
JExtor <JeffreyJExtor@gmail.com>
2020-11-13 21:51:41 -0500

    Fixed migrations file to simulate stakes in each pool

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -10,11 +10,30 @@ const LP = artifacts.require("mocks/DemoLP")
 
 module.exports = async function (deployer, networks, accounts) {
 
+    // MOCKS //
+  //           //
     let rewardTokensBefore = 60000; // 60,000
+    rewardTokens = web3.utils.toWei(rewardTokensBefore.toString(), 'ether');
+
+    initialBalanceBefore = 1000
+    allowanceBefore = 2000;
+    stakeAmountBefore = 5;
+    rewardTokensBefore = 60000
+
+    initialBalance = web3.utils.toWei(initialBalanceBefore.toString(), 'ether');
+    allowance = web3.utils.toWei(allowanceBefore.toString(), 'ether');
+    stakeAmount = web3.utils.toWei(stakeAmountBefore.toString(), 'ether');
     rewardTokens = web3.utils.toWei(rewardTokensBefore.toString(), 'ether');
 
     // Owner address
     const owner = accounts[1];
+
+    const user = accounts[3];
+
+    const user2 = accounts[4];
+
+    const user3 = accounts[5];
+
 
     // Deploy token
     await deployer.deploy(Token);
@@ -54,12 +73,48 @@ module.exports = async function (deployer, networks, accounts) {
     await rewardPool.allowTransferToStaking(nfyStaking.address, "11579208923731619542357098500868790785326998");
     await rewardPool.allowTransferToStaking(lpStaking.address, "11579208923731619542357098500868790785326998");
 
+    // DEMO LP token faucet
+    await lp.faucet(user, initialBalance);
+    await lp.faucet(user2, initialBalance);
+    await lp.faucet(user3, initialBalance);
+
+    // Approve LP staking address
+    await lp.approve(lpStaking.address, allowance, {from: user});
+    await lp.approve(lpStaking.address, allowance, {from: user2});
+    await lp.approve(lpStaking.address, allowance, {from: user3});
+
+    // DEMO NFY faucet
+    await token.faucet(user, initialBalance);
+    await token.faucet(user2, initialBalance);
+    await token.faucet(user3, initialBalance);
+
+    // Approve NFY staking address
+    await token.approve(nfyStaking.address, allowance, {from: user});
+    await token.approve(nfyStaking.address, allowance, {from: user2});
+    await token.approve(nfyStaking.address, allowance, {from: user3});
+
     // Transfer ownership to secured secured account
     await nfyStakingNFT.transferOwnership(owner);
     await lpStakingNFT.transferOwnership(owner);
     await nfyStaking.transferOwnership(owner);
     await lpStaking.transferOwnership(owner);
     await rewardPool.transferOwnership(owner);
+
+    // Stake DEMO LP Tokens
+    await lpStaking.stakeLP(stakeAmount, {from: user});
+    await lpStaking.stakeLP(stakeAmount, {from: user2});
+    await lpStaking.stakeLP(stakeAmount, {from: user3});
+
+    // Stake DEMO NFY Tokens
+    await nfyStaking.stakeNFY(stakeAmount, {from: user});
+    await nfyStaking.stakeNFY(stakeAmount, {from: user2});
+    await nfyStaking.stakeNFY(stakeAmount, {from: user3});
+
+    // END MOCKS  //
+      //       //
+
+
+    // TRADING PLATFORM //
 
     // Deploy Trading Platform
     await deployer.deploy(NFYTradingPlatform);
